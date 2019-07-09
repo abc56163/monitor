e52996cb3011abcb1c6d7f11106e8604884a550b
abc56163 <46206579+abc56163@users.noreply.github.com>
2019-07-09 15:00:38 +0800

    Update settings.py

--- monitor/settings.py
+++ monitor/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'monitor',
         'USER': 'root',
-        'PASSWORD': 'Abcd520025@',
+        'PASSWORD': '*********',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
